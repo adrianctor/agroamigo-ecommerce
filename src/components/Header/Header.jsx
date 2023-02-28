247dd7a24b1cefcbeb30bf29617041f9ce437b0f
Adrian Torres <adctg2799@gmail.com>
2023-02-28 17:52:04 -0500

    Agregando sticky header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
   const headerRef = useRef(null);
   const stickyHeaderFunc = ()=>{
     window.addEventListener('scroll',()=>{
-      if (document.body.scrollTo > 80 || document.documentElement.scrollTo > 80) {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         headerRef.current.classList.add("sticky__header");
       } else{
         headerRef.current.classList.remove("sticky__header");
